c90ef29e54a5c78e181922d2adb542e3b2ef391a
Sheldon <webtestingrun15@users.noreply.github.com>
2025-09-05 14:34:20 -0400

    feat: operate function with numbers and operators

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ function multiply(a,b) {
 function divide(a,b) {
   return a / b;
 }
+
+function operate(firstNumber,operator,secondNumber){
+  if (operator === '+') {
+    return add(firstNumber,secondNumber);
+  }
+  if (operator === '-') {
+    return subtract(firstNumber, secondNumber);
+  }
+  if (operator === '*') {
+    return multiply(firstNumber, secondNumber);
+  }
+  if (operator === '/') {
+    return divide(firstNumber, secondNumber);
+  }
+}
